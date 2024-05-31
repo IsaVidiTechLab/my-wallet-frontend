51c66254c957279ac87b4f522e896d1faf23082a
Vidisha Shah <vidisha.shah14@gmail.com>
2024-05-31 17:16:19 +0200

    Sign up CSS done

--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -2,10 +2,9 @@ import { useState, useContext } from "react";
 import axios from "axios";
 import { Link, useNavigate } from "react-router-dom";
 import { AuthContext } from "../context/auth.context";
-import "../style/Login.css"
+import "../style/Login.css";
 
-
-const API_URL = import.meta.env.VITE_API_URL
+const API_URL = import.meta.env.VITE_API_URL;
 
 function LoginPage(props) {
   const [email, setEmail] = useState("");
@@ -35,53 +34,78 @@ function LoginPage(props) {
   };
 
   return (
-    <div class="flex min-h-full flex-col justify-center px-6 py-12 lg:px-8 bgcolor-dashboard">
-  <div class="sm:mx-auto sm:w-full sm:max-w-sm">
-    <h2 class="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-color-login">Login to My Wallet</h2>
-  </div>
-
-  <div class="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
-    <form class="space-y-6" onSubmit={handleLoginSubmit}>
-      <div>
-        <label for="email" class="block text-sm font-medium leading-6 text-color-login ">Email address</label>
-        <div class="mt-2">
-          <input 
-            type="email"
-            name="email"
-            value={email}
-            onChange={handleEmail}
-            autocomplete="email" 
-            required 
-            class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"/>
-        </div>
+    <div className="flex min-h-full flex-col justify-center items-center  px-6 py-12 lg:px-8 bg-midnight">
+    <div className="md:w-1/3 p-6 bg-white border border-gray-200 rounded-lg shadow">
+      <div className="sm:mx-auto sm:w-full sm:max-w-sm">
+        <h2 className="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-darkgray">
+          Login to My Wallet
+        </h2>
       </div>
 
-      <div>
-        <label for="password" class="block text-sm font-medium leading-6 text-color-login">Password</label>
-        <div class="mt-2">
-          <input 
-            type="password"
-            name="password"
-            value={password}
-            onChange={handlePassword} 
-            autocomplete="current-password" 
-            required 
-            class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"/>
-        </div>
-      </div>
+      <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
+        <form className="space-y-6" onSubmit={handleLoginSubmit}>
+          <div>
+            <label
+              htmlFor="email"
+              className="block text-sm font-medium leading-6 text-darkgray "
+            >
+              Email address
+            </label>
+            <div className="mt-2">
+              <input
+                type="email"
+                name="email"
+                value={email}
+                onChange={handleEmail}
+                autoComplete="email"
+                required
+                className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+              />
+            </div>
+          </div>
 
-      <div>
-        <button type="submit" class="flex w-full justify-center rounded-md btn-color-login px-3 py-1.5 text-sm font-semibold leading-6 text-color shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">Sign in</button>
-      </div>
-    </form>
+          <div>
+            <label
+              htmlFor="password"
+              className="block text-sm font-medium leading-6 text-darkgray"
+            >
+              Password
+            </label>
+            <div className="mt-2">
+              <input
+                type="password"
+                name="password"
+                value={password}
+                onChange={handlePassword}
+                autoComplete="current-password"
+                required
+                className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+              />
+            </div>
+          </div>
+          <br />
+          <div>
+            <button
+              type="submit"
+              className="flex w-full justify-center rounded-md bg-lightblue hover:bg-opacity-80 px-3 py-1.5 text-sm font-semibold leading-6 text-midnight shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+            >
+              Sign in
+            </button>
+          </div>
+        </form>
 
-    <p class="mt-10 text-center text-sm text-color-login">
-      Not a member?
-      <a href="#" class="font-semibold leading-6 text-indigo-600 hover:text-indigo-500"> Sign Up</a>
-    </p>
-  </div>
-</div>
-    
+        <p className="mt-10 text-center text-sm text-darkgray ">
+          Not a member?
+          <Link
+            to={"/signup"}
+            className="font-semibold leading-6 text-indigo-600 hover:text-indigo-500"
+          >
+             {" "}Sign Up
+          </Link>
+        </p>
+      </div>
+      </div>
+    </div>
   );
 }
 
--- src/pages/SignupPage.jsx
+++ src/pages/SignupPage.jsx
@@ -32,48 +32,98 @@ function SignupPage(props) {
   };
 
   return (
-    <div className="SignupPage">
-      <h1 className="title-sign-up">Sign Up</h1>
-      <br />
-      <form onSubmit={handleSignupSubmit} className="form-sign-up">
-        <label className="email-label">Email:</label>
-        <input
-          className="email-input"
-          type="email"
-          name="email"
-          value={email}
-          onChange={handleEmail}
-        />
+    <div className="flex min-h-full flex-col justify-center items-center  px-6 py-12 lg:px-8 bg-midnight">
+    <div className="md:w-1/3 p-6 bg-white border border-gray-200 rounded-lg shadow">
+      <div className="sm:mx-auto sm:w-full sm:max-w-sm">
+        <h2 className="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-darkgray">
+          SignUp to My Wallet
+        </h2>
+      </div>
 
-        <label className="pw-label">Password:</label>
-        <input
-          className="pw-input"
-          type="password"
-          name="password"
-          value={password}
-          onChange={handlePassword}
-        />
-        <label className="name-label">Name:</label>
-        <input
-          className="name-input"
-          type="text"
-          name="name"
-          value={name}
-          onChange={handleName}
-        />
+      <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
+        <form className="space-y-6" onSubmit={handleSignupSubmit}>
+        <div>
+            <label
+              htmlFor="email"
+              className="block text-sm font-medium leading-6 text-darkgray "
+            >
+              Name
+            </label>
+            <div className="mt-2">
+              <input
+                type="text"
+                name="name"
+                value={name}
+                onChange={handleName}
+                autoComplete="email"
+                required
+                className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+              />
+            </div>
+          </div>
+          <div>
+            <label
+              htmlFor="email"
+              className="block text-sm font-medium leading-6 text-darkgray "
+            >
+              Email address
+            </label>
+            <div className="mt-2">
+              <input
+                type="email"
+                name="email"
+                value={email}
+                onChange={handleEmail}
+                autoComplete="email"
+                required
+                className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+              />
+            </div>
+          </div>
 
-        <button type="submit" className="btn-sign-up">
-          Sign Up
-        </button>
-      </form>
-    <br />
-    <br />
-    
-      <p className="p-sign-up">Already have account?</p>
-      <br />
-     
-      <button className="btn-login-sign-up"><Link to={"/login"}> Login</Link></button>
+          <div>
+            <label
+              htmlFor="password"
+              className="block text-sm font-medium leading-6 text-darkgray"
+            >
+              Password
+            </label>
+            <div className="mt-2">
+              <input
+                type="password"
+                name="password"
+                value={password}
+                onChange={handlePassword}
+                autoComplete="current-password"
+                required
+                className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+              />
+            </div>
+          </div>
+          <br />
+          <div>
+            <button
+              type="submit"
+              className="flex w-full justify-center rounded-md bg-lightblue hover:bg-opacity-80 px-3 py-1.5 text-sm font-semibold leading-6 text-midnight shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+            >
+              Sign Up
+            </button>
+          </div>
+        </form>
+
+        <p className="mt-10 text-center text-md text-darkgray ">
+          Not a member?
+          <Link
+            to={"/login"}
+            className="font-semibold leading-6"
+          >
+             {" "}Login
+          </Link>
+        </p>
+      </div>
+      </div>
     </div>
+    
   );
 }
 
