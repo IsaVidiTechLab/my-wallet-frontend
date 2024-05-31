e4d1cfb693879b797051a57552f618cb53ab03c4
Isabel Robleda <isabel.robleda@gmail.com>
2024-05-31 17:30:48 +0200

    changes in sidebar to isLoggedin

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,67 +1,87 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import { FaChalkboard, FaFolder, FaSignOutAlt, FaChartPie } from "react-icons/fa";
+import {
+  FaChalkboard,
+  FaFolder,
+  FaSignOutAlt,
+  FaChartPie,
+} from "react-icons/fa";
 import "../style/Sidebar.css";
 import { useContext } from "react";
 import { AuthContext } from "../context/auth.context";
 
 function Sidebar() {
+  const { isLoggedIn, logOutUser } = useContext(AuthContext);
 
-    const { isLoggedIn, logOutUser } = useContext(AuthContext);
-
-    const handleLogout = () => {
-        logOutUser();
-        setDropdownOpen(false);
-      };
+  const handleLogout = () => {
+    logOutUser();
+    setDropdownOpen(false);
+  };
 
   return (
-    <div className="sidebar bg-darkgray text-white pl-5 pt-10 flex flex-col h-full">
-      <div className="flex-grow">
-        <br />
-        <div className="flex pb-5">
-          <Link to={"/expense"} className="flex flex-row items-center">
-            <div className="pr-2">
-              <FaChalkboard />
+    <>
+      {isLoggedIn && (
+        <div className="sidebar bg-darkgray text-white pl-5 pt-10 flex flex-col h-full">
+          <div className="flex-grow">
+            <br />
+            <div className="flex pb-5">
+              <Link to={"/expense"} className="flex flex-row items-center">
+                <div className="pr-2">
+                  <FaChalkboard />
+                </div>
+                <div className="">Dashboard</div>
+              </Link>
             </div>
-            <div className="">Dashboard</div>
-          </Link>
-        </div>
 
-        <div className="flex pb-5">
-          <Link to={"/category"} className="flex flex-row items-center">
-            <div className="pr-2">
-              <FaFolder />
+            <div className="flex pb-5">
+              <Link to={"/category"} className="flex flex-row items-center">
+                <div className="pr-2">
+                  <FaFolder />
+                </div>
+                Categories
+              </Link>
             </div>
-            Categories
-          </Link>
-        </div>
-        <div className="flex pb-5">
-          <Link to={"/monthly-report"} className="flex flex-row items-center">
-            <div className="pr-2">
-              <FaChartPie />
+            <div className="flex pb-5">
+              <Link
+                to={"/monthly-report"}
+                className="flex flex-row items-center"
+              >
+                <div className="pr-2">
+                  <FaChartPie />
+                </div>
+                Monthly Report
+              </Link>
             </div>
-            Monthly Report
-          </Link>
-        </div>
-        <div className="flex">
-          <Link to={"/yearly-report"} className="flex flex-row items-center">
-            <div className="pr-2">
-              <FaChartPie />
+            <div className="flex">
+              <Link
+                to={"/yearly-report"}
+                className="flex flex-row items-center"
+              >
+                <div className="pr-2">
+                  <FaChartPie />
+                </div>
+                Yearly Report
+              </Link>
             </div>
-            Yearly Report
-          </Link>
-        </div>
-        <br />
-      </div>
-      <div className="flex mt-auto">
-        {logOutUser && (<Link to={"/logout"} className="flex flex-row items-center pb-4" onClick={handleLogout}>
-          <div className="pr-2">
-            <FaSignOutAlt />
+            <br />
           </div>
-          Logout
-        </Link>)}
-      </div>
-    </div>
+          <div className="flex mt-auto">
+            {logOutUser && (
+              <Link
+                to={"/logout"}
+                className="flex flex-row items-center pb-4"
+                onClick={handleLogout}
+              >
+                <div className="pr-2">
+                  <FaSignOutAlt />
+                </div>
+                Logout
+              </Link>
+            )}
+          </div>
+        </div>
+      )}
+    </>
   );
 }
 
